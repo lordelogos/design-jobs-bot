f575ccfb8805cf64b7b7ad6e1c24687ec2e95236
Paul Ehikhuemen <paulehiks@gmail.com>
2021-01-28 19:39:57 +0100

    added gitignore

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key: "MBf97uw62Sk7YFmZRm9LJxJwe",
-	consumer_secret: "PTDCIWrwlDmCAE5erskYCgpLvouswGv9pxmQujubMQ7E0SHa88",
-	access_token: "1352615813254164489-8jXzJoqK6eo9w4lrUK5rxEpKJkcB9T",
-	access_token_secret: "AetGF32hHggprCubagtd2YflMzq1UrSvGYendGL4KDiKN",
-};
